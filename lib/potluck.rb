4c04306f8a44df68f3145ba9fdfce51beb105df1
Max Mitrani <mjmitrani168@gmail.com>
2020-03-25 08:46:52 -0600

    Add get_all_from_catagory method and tests

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -10,4 +10,14 @@ attr_reader :date, :dishes
     @dishes << dish
   end
 
+  def get_all_from_category(type)
+    all_of_catagory = []
+    @dishes.each do |dish|
+      if dish.catagory == type
+        all_of_catagory << dish
+      end
+    end
+    return all_of_catagory
+  end
+
 end
